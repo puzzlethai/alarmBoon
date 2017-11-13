2f2f6183fb0ed9881fa471e4c6cf0acfe4d58e80
OZONE <iq_maze@yahoo.com>
2017-11-13 11:13:50 +0700

    Ver 5 add @bean mongoDBtemplete

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkApplication.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkApplication.java
@@ -23,6 +23,13 @@ import java.nio.file.Path;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.MongoDbFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
 
 @SpringBootApplication
 @EnableScheduling
@@ -33,5 +40,19 @@ public class KitchenSinkApplication {
         downloadedContentDir = Files.createTempDirectory("line-bot");
         SpringApplication.run(KitchenSinkApplication.class, args);
     }
+    @Bean
+    public MongoTemplate mongoTemplate(MongoDbFactory mongoDbFactory,
+                                       MongoMappingContext context) {
+
+        MappingMongoConverter converter =
+                new MappingMongoConverter(new DefaultDbRefResolver(mongoDbFactory), context);
+        converter.setTypeMapper(new DefaultMongoTypeMapper(null));
+
+        MongoTemplate mongoTemplate = new MongoTemplate(mongoDbFactory, converter);
+
+        return mongoTemplate;
+
+    }
 
 }
+
