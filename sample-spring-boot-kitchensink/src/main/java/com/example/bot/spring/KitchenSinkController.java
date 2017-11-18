2a5f66047408beac90292aec5108031c09656976
OZONE <iq_maze@yahoo.com>
2017-11-18 20:53:14 +0700

    Ver 5 forcast LineNotify at Ogust Group

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
@@ -500,7 +500,7 @@ public class KitchenSinkController {
                 count = "NULL";
             } else {
                 count = obj2.getDomain();
-                final String USER_TOKEN = "rICuSZ1ucyHglz3trm1NJpGUKvl6mT076y05Hy3CVjy";
+                final String USER_TOKEN = "AX5dArSRT3QI1ujMSph8oQCaQd5UuwqxI877fuuSzfl";
                 LineNotify ln = new LineNotify(USER_TOKEN);
                 try{
                     ln.notifyMe("Tomorrow is วันพระ");
