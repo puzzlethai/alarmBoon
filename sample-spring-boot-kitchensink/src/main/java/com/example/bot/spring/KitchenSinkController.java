519e846c8369cc27462f52f01dbd9c6b86cb9937
OZONE <iq_maze@yahoo.com>
2017-11-20 13:32:51 +0700

    Ver 3.5 try use only pushT

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
@@ -303,7 +303,7 @@ public class KitchenSinkController {
                 } else {
                     this.replyText(replyToken, "Bot can't use profile API without user ID");
                 }
-               // this.pushText(userId,"test pushText to ");
+               this.pushT(userId,(new TextMessage("test pushText to ")));
                // this.pushText(userId,userId);
                 break;
             }
