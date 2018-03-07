d358f9eedcdab8367cdfabc9f764092144aaab98
OZONE <iq_maze@yahoo.com>
2018-03-07 11:29:38 +0700

    Ver 8.1 testOilPrice add header.java and item.java

---
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/Header.java
@@ -0,0 +1,144 @@
+package com.example.bot.spring;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Locale;
+
+@XmlRootElement(name = "header")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Header {
+    @XmlElement(name = "title")
+    private String title;
+
+    @XmlElement(name = "description")
+    private String description;
+
+    @XmlElement(name = "copyright")
+    private String copyright;
+
+    @XmlElement(name = "update_date")
+    private String update_date;
+
+    @XmlElement(name = "price_link")
+    private String price_link;
+
+    @XmlElement(name = "remark_th")
+    private String remark_th;
+
+    @XmlElement(name = "remark_en")
+    private String remark_en;
+
+    @XmlElement(name = "bankchak_logo")
+    private String bankchak_logo;
+
+    @XmlElement(name = "item")
+    private List<Item> items;
+
+    public void checkDup(){
+        List<Item> items = this.items;
+        Integer checkDup = 0;
+        checkDup = items.size();
+        System.out.println("checkDup :"+checkDup);
+        if (checkDup == 14) {
+            items.remove(0);
+            items.remove(0);
+            items.remove(0);
+            items.remove(0);
+            items.remove(0);
+            items.remove(0);
+            items.remove(0);
+        }
+        System.out.println("size :"+this.items.size());
+    }
+    @Override
+    public String toString() {
+
+        List<Item> items = this.items;
+        String temp = " ";
+        int i = 0;
+        for (Item item : items) {
+            temp = temp+ item.toString()+String.valueOf(i++);
+        }
+        return temp;
+    }
+
+    public Boolean isSame() {
+        List<Item> items = this.items;
+        Boolean temp = Boolean.TRUE;
+        for (Item item : items) {
+            temp = temp&&item.isNotChange();
+            item.setChange();
+        }
+        return temp;
+    }
+
+
+    public String showHTML(){
+        List<Item> items = this.items;
+        Locale lc = new Locale("th","TH");
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d MMM uuuu",lc);
+
+        String todayStr = now.format(formatter);
+        String tempStr =  "<!DOCTYPE html><html lang=\"th\"><head><meta charset=\"UTF-8\"><style type=\"text/css\">th {border-bottom: 1px solid #ddd;padding: 15px;text-align: center;}"+
+                "td {color:black; border-bottom: 1px solid #9cff8b;padding: 15px;text-align: center;}"+
+                "table {} .blue{\n" +
+                "        color:blue;\n" +
+                "        background: none;\n" +
+                "    }</style></head>" +
+                "<body style=\"font-family: tahoma;font-size: large\">"+ //MS Sans Serif,DB ThaiTextFixed
+                "<table cellspacing=\"0\" >"+
+                "<tr bgcolor=#57b33e ><td colspan=\"4\" style=\"padding: 10px;color:white\"><h2>ราคาน้ำมัน by AlarmBoon</h2></td></tr>"+
+                "<tr style=\"color:blue\">" +
+                "<th>"+todayStr+"</th>" +
+                "<th>วันนี้</th>" +
+                "<th>พรุ่งนี้</th>" +
+                "<th>ส่วนต่าง</th>" +
+                "</tr><tr bgcolor=\"orange\"><th>ชนิดน้ำมัน</th>" +
+                "<th>บาท/ลิตร</th>" +
+                "<th>บาท/ลิตร</th>" +
+                "<th>บาท</th></tr>";
+
+        for (Item item : items) {
+            if (item.getoilType().equals("NGV")){
+                tempStr = tempStr + "</tr><tr bgcolor=\"orange\"><th>ชนิดน้ำมัน</th>" +
+                        "<th>บาท/กก.</th>" +
+                        "<th>บาท/กก.</th>" +
+                        "<th>บาท</th></tr>";
+            }
+            String colorStr = "";
+            String boldBegin = "";
+            String boldEnd = "";
+            switch (item.getChange().substring(0,1)){
+                case "+": colorStr = "style=\"color:red\"";
+                    boldBegin = "<b>";
+                    boldEnd = "</b>";
+                    break;
+                case "-": colorStr = "style=\"color:#1fa743\""; // #1fa743 #3cb371 #0bb344 #32cd32
+                    boldBegin = "<b>";
+                    boldEnd = "</b>";
+                    break;
+                /*default: colorStr = "style=\"color:blue\"";
+                    boldBegin = "<b>";
+                    boldEnd = "</b>";*/
+            }
+
+            tempStr = tempStr + "<tr><td>"+item.getoilType()+"</td>"+
+                    "<td>"+item.gettoday()+"</td>"+
+                    "<td "+colorStr+">"+boldBegin+item.gettomorrow()+boldEnd+"</td>"+
+                    "<td "+colorStr+">"+boldBegin+item.getChange()+boldEnd+"</td></tr>";
+        }
+
+        tempStr = tempStr + "<tr style=\"border-bottom: 0px;\"><th colspan=\"4\" style=\"padding: 10px;border-bottom: 0px;\">" +
+                "เตือนราคาน้ำมันฟรีแค่ Add LINE &nbsp <span class=\"blue\">@hpd8343b</span></th></tr>"+
+                "<td colspan=\"4\" style=\"padding: 0px;border-bottom: 0px;\"><h6>ขอบคุณข้อมูลจาก บางจาก</h6>"+
+                "</table></body></html>";
+        return tempStr;
+    }
+}
---
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/Item.java
@@ -0,0 +1,122 @@
+package com.example.bot.spring;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+// Controls whether fields or Javabean properties are serialized by default.
+// By default all public members will be searialized,
+// XmlAccessType.FIELD Serializes using FIELDS
+
+// Use @XmlRootElement to customize the XML root tag, by default it defaults
+// to class name 'Employee'
+
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Item {
+    // Below annotation is to customize the oilType XML tag
+    @XmlElement(name = "type")
+    private String oilType;
+
+    private String today;
+
+    private String tomorrow;
+
+    // private String designation;
+    private String image;
+    private String image2;
+    private String unit_th;
+    private String unit_en;
+    private String change;
+
+    public Item() {
+    }
+
+    public Item(String oilType,String today, String tomorrow, String image, String image2, String unit_th, String unit_en) {
+        super();
+        this.oilType = oilType;
+        this.today = today;
+        this.tomorrow = tomorrow;
+        this.image = image;
+        this.image2 = image2;
+        this.unit_th = unit_th;
+        this.unit_en = unit_en;
+    }
+
+    public String getoilType() {
+        return oilType;
+    }
+
+    public void setoilType(String oilType) {
+        this.oilType = oilType;
+    }
+
+    public String gettoday() {
+        return today;
+    }
+
+    public void settoday(String today) {
+        this.today = today;
+    }
+
+    public String gettomorrow() {
+        return tomorrow;
+    }
+
+    public void settomorrow(String tomorrow) {
+        this.tomorrow = tomorrow;
+    }
+
+    public String getimage() {
+        return image;
+    }
+
+    public void setimage(String image) {
+        this.image = image;
+    }
+
+    public void setimage2(String image2) {
+        this.image = image2;
+    }
+    public String getunit_th() {
+        return unit_th;
+    }
+
+    public void setunit_th(String unit_th) {
+        this.unit_th = unit_th;
+    }
+    public String getunit_en() {
+        return unit_en;
+    }
+
+    public void setunit_en(String unit_en) {
+        this.unit_en = unit_en;
+    }
+    public Boolean isNotChange(){
+        /*if (this.today == this.tomorrow)
+            return Boolean.TRUE;
+            else
+                return Boolean.FALSE;*/
+        return this.today.equals(this.tomorrow);
+    }
+    public void setChange(){
+        Double todayPrice = Double.valueOf(this.today);
+        Double tomorrowPrice = Double.valueOf(this.tomorrow);
+
+        if (this.today.equals(this.tomorrow)) {
+            this.change = "0.00";
+        } else if (todayPrice<tomorrowPrice) {
+            this.change = "+"+String.format("%.2f",(tomorrowPrice-todayPrice));
+        } else {
+            this.change = "-"+String.format("%.2f",(todayPrice-tomorrowPrice));
+        }
+    }
+    public String getChange(){
+        return change;
+    }
+    @Override
+    public String toString() {
+        return "item [oilType=" + oilType + ", today=" + today + ", tomorrow=" + tomorrow + ", change= " + change + ", unit_th="
+                + unit_th + "]";
+    }
+}
