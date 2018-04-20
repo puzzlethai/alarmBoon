5158028ea76d7d9a90b8d55f1dd26ce28da3e7ab
OZONE <iq_maze@yahoo.com>
2018-04-20 11:31:24 +0700

    8.15 createURI

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/WebImage.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/WebImage.java
@@ -3,6 +3,8 @@ package com.example.bot.spring;
 //Class that Converts the web page to Image
 //import lombok.extern.slf4j.Slf4j;
 
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
 import java.awt.*;
 import java.awt.image.*;
 import java.io.FileInputStream;
@@ -23,7 +25,11 @@ public abstract class WebImage
             return doc;
         }
     }
-
+    private static String createUri(String path) {
+        return ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path(path).build()
+                .toUriString();
+    }
     public static BufferedImage create
             (String src, int width, int height) {
         BufferedImage image = null;
@@ -35,8 +41,8 @@ public abstract class WebImage
         pane.setEditable(false);
         pane.setMargin(new Insets(0,0,0,0));
         try {
-
-            FileInputStream fis = new FileInputStream( "/static/buttons/TH Krub.ttf");
+            String fontUrl = createUri("/static/buttons/TH Krub.ttf");
+            FileInputStream fis = new FileInputStream( fontUrl);
             Font fontT = Font.createFont(Font.TRUETYPE_FONT, fis);
             //pane.setPage(src);
             //pane.putClientProperty(JEditorPane.HONOR_DISPLAY_PROPERTIES, Boolean.TRUE);
